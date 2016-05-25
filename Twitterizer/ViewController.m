f0a96411d752db98d687cf5747a2f5a7e08121da
Ernie Barojas <ernie.barojas@gmail.com>
2016-05-25 15:59:12 -0500

    Fifth Commit

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -8,8 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextViewDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
 
 @end
 
@@ -22,12 +24,57 @@
 
 
 - (IBAction)twitterizeButton:(id)sender {
-    NSString *enteredText = self.textView.text;
-    NSMutableString *twitterizeOutput = [[NSMutableString alloc] init];
     
-    for (int i = 0; i < self.textView.text.length - 1; i++) {
-         
-    }
+    //step 2: grab nsstring from the uitextview
+    NSString *enteredText = self.textView.text;
+    
+    //step2: loop over characters in nsstring and remove vowels
+    //    NSMutableString *twitterizeOutput = [[NSMutableString alloc] init];
+    
+    // we added -1 to the condition due to the index starting at 0, otherwise we would miss the last character in the textView.
+    
+// original for loop idea:
+//    for (int i = 0; i <= self.textView.text.length - 1; i++) {
+    
+    NSString *stringWithoutA = [enteredText stringByReplacingOccurrencesOfString:@"a" withString:@""];
+    
+    NSLog(@"without A - %@",stringWithoutA);
+    
+    NSString *stringWithoutE = [stringWithoutA stringByReplacingOccurrencesOfString:@"e" withString:@""];
+
+    NSLog(@"without E - %@", stringWithoutE);
+    
+    NSString *stringWithoutI = [stringWithoutE stringByReplacingOccurrencesOfString:@"i" withString:@""];
+    
+    NSLog(@"without I - %@", stringWithoutI);
+    
+    NSString *stringWithoutO = [stringWithoutI stringByReplacingOccurrencesOfString:@"o" withString:@"xxxxx"];
+    
+    NSLog(@"without O - %@", stringWithoutO);
+    
+    NSString *stringWithoutU = [stringWithoutO stringByReplacingOccurrencesOfString:@"u" withString:@""];
+    
+    NSLog(@"without U - %@", stringWithoutU);
+    
+    self.textView.text = stringWithoutU;
+    
+    
+    
+//        if ([enteredText characterAtIndex:i] ="a" || [enteredText characterAtIndex:i] = "E") {
+//            ;
+//        }
+//        
+//        NSLog(@"%c",character);
+//        
+    
+//    }
 }
+
+-(void)textViewDidChange:(UITextView *)textView {
+
+    NSLog(@"hi");
+
+}
+
 
 @end
