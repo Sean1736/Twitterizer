d4d81359547703a495c74a8953d407ec27d306d1
Ernie Barojas <ernie.barojas@gmail.com>
2016-05-25 14:45:28 -0500

    Second Commit

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  Twitterizer
 //
-//  Created by Salar Kohnechi on 5/25/16.
+//  Created by Ernie Barojas on 5/25/16.
 //  Copyright © 2016 Salar Kohnechi. All rights reserved.
 //
 
